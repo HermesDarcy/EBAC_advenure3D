faef5ca9aaf2739490a337b940fea5d69d667149
HermesDarcy <eng.hdbm@gmail.com>
2025-06-15 08:43:52 -0300

    Correção e adição
    adição do stateMachine no Player
    Remoção cena 2D

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,18 +6,61 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float forceJump;
+    public float forceJump =10f;
+    public float speed=5f;
+    public float rotSpeed = 10f;
+    public Animator playerAnim;
     private Rigidbody rb;
 
-    private void Start()
+
+
+    public enum states
     {
-        rb = GetComponent<Rigidbody>();
+        Init,
+        Attack,
+        Run,
+        Idle,
+        Jump,
+        Walk,
+        WalkTras,
+        RotLeft,
+        RotRight,
+        Death
     }
 
-    private void Update()
+    public states onState;
+    public StateMachine<states> posStateMachine = new StateMachine<states>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        posStateMachine.Init();
+        posStateMachine.RegisterStates(states.Init, new StateBase());
+        posStateMachine.RegisterStates(states.Attack, new StatePlayerAttack());
+        posStateMachine.RegisterStates(states.Idle, new StateBase());
+        posStateMachine.RegisterStates(states.Run, new StateBase());
+        posStateMachine.RegisterStates(states.Jump, new StatePlayerJump());
+        posStateMachine.RegisterStates(states.Walk, new StateWalk());
+        posStateMachine.RegisterStates(states.WalkTras, new StateWalkTras());
+        posStateMachine.RegisterStates(states.RotLeft, new StateBase());
+        posStateMachine.RegisterStates(states.RotRight, new StateBase());
+        posStateMachine.RegisterStates(states.Death, new StateBase());
+
+
+        //posStateMachine.SwithState(onState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posStateMachine.SwithState(onState);
         moves();
     }
+
+
 
 
     private void moves()
@@ -25,21 +68,44 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * forceJump, ForceMode.Impulse);
+            onState = states.Jump;
             Debug.Log("Pulo Player");
         }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
+            onState = states.Walk;
+            transform.position += transform.forward * speed * Time.deltaTime;
+            playerAnim.SetTrigger("Walk");
             Debug.Log("walk Player");
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            onState = states.RotLeft;
+            playerAnim.SetTrigger("Idle");
+            transform.Rotate(Vector3.up, -rotSpeed * Time.deltaTime);
             Debug.Log("left rotate  Player");
 
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
+            onState = states.RotRight;
+            playerAnim.SetTrigger("Idle");
+            transform.Rotate(Vector3.up, +rotSpeed * Time.deltaTime);
             Debug.Log("right rotate Player");
         }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            onState = states.WalkTras;
+            playerAnim.SetTrigger("BackWalk");
+            transform.position -= transform.forward * speed * 0.8f * Time.deltaTime;
+        }
+        else
+        {
+            //onState = states.Idle;
+            playerAnim.SetTrigger("Idle");
+
+        }
+
     }
 
 }
---
+++ Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    
+}
+
+
+public class StatePlayerAttack : StateBase
+{
+
+}
+
+public class StateWalk : StateBase
+{
+
+}
+
+public class StatePlayerJump : StateBase
+{
+
+}
+
+public class StateWalkTras : StateBase
+{
+
+}
+
+
